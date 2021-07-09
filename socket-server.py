fb6cb51bdbbba1a81451d69c67c20446e6b266c2
emreharbutoglu <coder44278@gmail.com>
2021-07-09 22:52:41 +0300

    fix

--- socket-server.py
+++ socket-server.py
@@ -22,10 +22,10 @@ from array import array
 PAGE="""\
 <html>
 <head>
-<title>Raspberry Pi - Camera</title>
+<title>Canlı aktarım</title>
 </head>
 <body>
-<center><h1>Raspberry Pi - Camera</h1></center>
+<center><h1>Canlı aktarım</h1></center>
 <center><img src="stream.mjpg" width="640" height="480"></center>
 </body>
 </html>
