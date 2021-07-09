543c93b794fec1b24348c6fd4e1f21ec40f0fef4
emreharbutoglu <coder44278@gmail.com>
2021-07-09 18:00:15 +0300

    fix

--- rasp-cam.py
+++ rasp-cam.py
@@ -99,7 +99,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='320x240', framerate=12) as camera:
+with picamera.PiCamera(resolution='160x120', framerate=12) as camera:
     output = StreamingOutput()
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
--- socket-server.py
+++ socket-server.py
@@ -44,23 +44,6 @@ def rec():
         server.handle_request()
         print("data has been taken")
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -109,7 +92,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 try:
     thread = threading.Thread(target=rec, args=[])
     thread.start()
-    
+
     address = ('', 3306)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
