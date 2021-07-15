ca72117d46b34029803a98df7b06325405775326
emreharbutoglu <coder44278@gmail.com>
2021-07-15 16:58:30 +0300

    fix

--- rasp-cam.py
+++ rasp-cam.py
@@ -9,18 +9,6 @@ from http import server
 import io
 from PIL import Image
 
-PAGE="""\
-<html>
-<head>
-<title>Raspberry Pi - Surveillance Camera</title>
-</head>
-<body>
-<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
-</body>
-</html>
-"""
-
 serverAddressPort   = ('63.33.239.182', 3600)
 
 bufferSize          = 4096*2
@@ -44,51 +32,6 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header(
-                'Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                        
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-
-                    try:
-                        UDPClientSocket.sendto(frame, serverAddressPort)
-                    except Exception as e:
-                        print("error")
-            except Exception as e:
-                logging.warning('Removed streaming client %s: %s',self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
@@ -98,9 +41,18 @@ with picamera.PiCamera(resolution='80x60', framerate=12) as camera:
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
+
     try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
+        try:
+            while True:
+                with output.condition:
+                    output.condition.wait()
+                    frame = output.frame
+                try:
+                    UDPClientSocket.sendto(frame, serverAddressPort)
+                except Exception as e:
+                    print("error")
+        except Exception as e:
+            logging.warning('Removed streaming client %s: %s',self.client_address, str(e))
     finally:
         camera.stop_recording()
